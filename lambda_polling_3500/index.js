edfbd65593eec8f4c1dbe3880494277c7d0aa151
HanJongKo <rino0601.dev@gmail.com>
2018-03-25 23:09:24 +0900

    scan 범위 지정을 위해서는, secondary index가 필요하길래, 추가시에 필요한 작업 수행. 및 필터링 적용.

--- lambda_polling_3500/index.js
+++ lambda_polling_3500/index.js
@@ -3,6 +3,7 @@ const uuidv4 = require('uuid/v4');
 const AWS = require('aws-sdk');
 const DOC = require('dynamodb-doc');
 const ddb = new DOC.DynamoDB();
+const moment = require('moment');
 
 const putItemPromise = (params) => {
     return new Promise((resolve, reject) => {
@@ -18,18 +19,39 @@ const putItemPromise = (params) => {
 
 
 exports.handler = function (event, context) {
+
     const apiUrl = "https://ssl.map.naver.com/api.pubtrans/2.2/live/getBusLocation.jsonp?caller=mobile_web&routeId=10596";
-    fetch(apiUrl).then(value => value.json()).then(res => {
-        return putItemPromise({
-            TableName: 'lambda_polling_bus_raw',
-            Item: {
-                "documentId": uuidv4(),
-                "message": res.message,
-            }
+    fetch(apiUrl).then(value => value.json()).then((data) => {
+        const message = data.message;
+        if (!message.result) {
+            context.done("no result. skip it.");
+            return;
+        }
+        const {updateDate, routeId, busLocationList} = message.result;
+        const upperUpdateDate = updateDate;
+        const epochTime = moment(upperUpdateDate, "YYYYMMDDhhmmss").valueOf();  // 1522019222000 이후 부터 쌓이기 시작
+        const dataList = busLocationList.map(bus => {
+            const {plateNo, remainSeat, stateCd, stationId, stationSeq, updateDate} = bus;
+            return {
+                documentId: uuidv4(),
+                epochTime,
+                routeId,
+                plateNo,
+                upperUpdateDate,
+                remainSeat,
+                stateCd,
+                stationId,
+                stationSeq,
+                offset: upperUpdateDate - updateDate
+            };
         });
-    }).then(data => {
-        console.log("Success", data);
-        context.done(null, data);
+        return Promise.all(dataList.map(data => putItemPromise({
+            TableName: "lambda_polling_bus_refined",
+            Item: data,
+        }))).then(values => {
+            context.done("Done. new bus recodes ("+values.length+") are added.");
+            return undefined;
+        });
     }).catch(reason => {
         console.log(reason);
         context.fail('unable to fetch bus api at this time');
