11b67fce339c989c71accef4876c37f2f06e89e7
HanJongKo <rino0601.dev@gmail.com>
2018-04-04 01:12:21 +0900

    ETL 작성. 모두 받아와서 1초당 100개씩 밀어 넣는다. 예상 소요 30분~50분

--- lambda_extract/index.js
+++ lambda_extract/index.js
@@ -3,6 +3,7 @@ const uuidv4 = require('uuid/v4');
 const AWS = require('aws-sdk');
 const DOC = require('dynamodb-doc');
 const ddb = new DOC.DynamoDB();
+const moment = require('moment');
 
 const putItemPromise = (params) => {
     return new Promise((resolve, reject) => {
@@ -28,26 +29,17 @@ const scanPromise = (params) => {
     });
 };
 
-
-exports.handler = function (event, context) {
-    scanPromise({
-        TableName: 'lambda_polling_bus_raw',
-        // Limit: 1500,
-        Limit: 3000,
-        FilterExpression: "attribute_exists(message.#r)",
-        ExpressionAttributeNames: {
-            "#r": "result"
-        },
-    }).then((data) => {
-        // console.log(data);
-        const chain = [];
+const fullScanReculsive = (params, chain, count, scannedCount) => {
+    return scanPromise(params).then((data) => {
         data.Items.map(item => {
             const {updateDate, routeId, busLocationList} = item.message.result;
             const upperUpdateDate = updateDate;
-            busLocationList.map(bus => {
+            const epochTime = moment(upperUpdateDate, "YYYYMMDDhhmmss").valueOf();
+            const dataList = busLocationList.map(bus => {
                 const {plateNo, remainSeat, stateCd, stationId, stationSeq, updateDate} = bus;
-                // console.log(exploded);
-                chain.push({
+                return {
+                    documentId: uuidv4(),
+                    epochTime,
                     routeId,
                     plateNo,
                     upperUpdateDate,
@@ -56,19 +48,69 @@ exports.handler = function (event, context) {
                     stationId,
                     stationSeq,
                     offset: upperUpdateDate - updateDate
-                });
+                };
             });
-        });
-        const noc = chain.length;
-        const filterd = chain.filter(ebus => {
-            return ebus.plateNo === "경기77바1335" && ebus.upperUpdateDate > 20180310040000 && ebus.upperUpdateDate < 20180311040000;
+            chain.push(...dataList);
         });
 
-        const nof = filterd.length;
-        filterd.sort((a, b) => a.upperUpdateDate - b.upperUpdateDate).map(ebus => {
-            console.log(ebus);
+        const countUpTo = count + data.Count;
+        const scannedCountUpTo = scannedCount + data.ScannedCount;
+        if (typeof data.LastEvaluatedKey !== "undefined") {
+            console.log("(" + countUpTo + "/" + scannedCountUpTo + ")Scanning for more...");
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            return fullScanReculsive(params, chain, countUpTo, scannedCountUpTo);
+        }
+        return [countUpTo, scannedCountUpTo];
+    })
+};
+
+const putItRecursive = (array, index) => {
+    if (index >= array.length) {
+        console.log("Done.");
+        return undefined;
+    }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 1000);
+    }).then(() => {
+        return Promise.all(array[index].map(data => putItemPromise({
+            TableName: "lambda_polling_bus_refined",
+            Item: data,
+        })));
+    }).then(((values) => {
+        console.log("New bus recodes (" + values.length + ") are added.");
+        return putItRecursive(array, index + 1);
+    }));
+};
+
+exports.handler = function (event, context) {
+    const params = {
+        TableName: 'lambda_polling_bus_raw',
+        FilterExpression: "attribute_exists(message.#r)",
+        ExpressionAttributeNames: {
+            "#r": "result"
+        },
+    };
+    const chain = [];
+    fullScanReculsive(params, chain, 0, 0).then(counts => {
+        const noc = chain.length;
+        const filtered = chain.filter(ebus => {
+            return ebus.epochTime < 1522019222000;
         });
-        context.done(nof + "/" + noc);
+        const nof = filtered.length;
+        console.log(nof + "/" + noc + " - " + counts[0] + "/" + counts[1]);
+        return filtered;
+    }).then(filtered => {
+        const range = n => [...Array(n).keys()];
+        const mod = Math.floor(filtered.length / 100);
+        const ticks = range(mod).map(n => []);
+        for (let i = 0; i < filtered.length; i++) {
+            ticks[i % mod].push(filtered[i]);
+        }
+        return putItRecursive(ticks, 0);
+    }).then(() => {
+        context.done(null, "Finally.");
     }).catch(reason => {
         console.log(reason);
         context.fail('unable to fetch bus api at this time');
