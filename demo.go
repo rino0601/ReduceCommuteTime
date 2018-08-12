bfa05dda18c3913430e06e5f20b0f0254d666988
rino0601 <rino0601.dev@gmail.com>
2018-08-12 22:41:44 +0900

    wip, start building go webserver for migration

--- demo.go
+++ demo.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"io/ioutil"
+)
+
 func main() {
-	
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000",nil))
+}
+func handler(writer http.ResponseWriter, request *http.Request) {
+	q := request.URL.Query().Get("q")
+	resp, _ := http.Get(q)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
+	fmt.Fprintf(writer, "URL.Path = %q\n", bytes)
 }
